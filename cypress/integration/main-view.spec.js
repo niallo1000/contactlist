091837ec3f47989ea6802479243cc1e09012efde
LAPTOP-QP7T4FMV\niall <20082280@mail.wit.ie>
2019-07-08 21:50:35 +0100

    E2E tests for filtering and  and main navigation

--- cypress/integration/main-view.spec.js
+++ cypress/integration/main-view.spec.js
@@ -80,5 +80,45 @@ describe("Main View ", () => {
                 });
         });
     });
+    describe("Filtering", () => {
+        it("filteres the contacts by name", () => {
+            cy.get("span")
+                .contains("Filter")
+                .next()
+                .type("la");
+            cy.get(".card").each($el => {
+                cy.wrap($el)
+                    .find(".card-title")
+                    .contains("la");
+            });
+        });
+
+        it("filters the contacts by gender", () => {
+            cy.get("select").select("Female");
+            cy.get(".card")
+                .its("length")
+                .then(females => {
+                    cy.get("select").select("Male");
+                    cy.get(".card")
+                        .its("length")
+                        .should("eq", 50 - females);
+                });
+        });
+    });
+    describe("Navigate to Contact page", () => {
+        it.only("goes to the correct view", () => {
+            cy.get("@targetcard")
+                .find("[data-icon=phone]")
+                .next()
+                .invoke("text")
+                .then(phoneNo => {
+                    cy.get("@targetcard")
+                        .find("img")
+                        .click();
+                    cy.url().should("include", `/contacts/${phoneNo.trim()}`);
+                    cy.get("span").contains(phoneNo.trim());
+                });
+        });
+    });
 
 });
