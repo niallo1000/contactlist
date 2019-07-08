ddd13578ca5b5415724308e1ec0124e0a416b52b
LAPTOP-QP7T4FMV\niall <20082280@mail.wit.ie>
2019-07-08 20:20:38 +0100

    Added nested route for private data

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from "react";
-import { withRouter } from "react-router-dom";
+//import { withRouter } from "react-router-dom";
 import api from "../dataStore/stubAPI"; // NEW
 import ContactPublic from "../components/contactPublic/";
 import ContactPrivate from "../components/contactPrivate/";
+import { withRouter, Route, Link } from "react-router-dom";
+
+
 
 const ContactPage = props => {
     const { id } = props.match.params;
@@ -12,7 +15,12 @@ const ContactPage = props => {
             {contact ? (
                 <Fragment>
                     <ContactPublic user={contact} />
-                    <ContactPrivate user={contact} />
+                    {!props.history.location.pathname.endsWith("/private") && (
+                        <Link class="btn btn-primary active" to={`/contacts/${id}/private`}>See Private Data</Link>
+                    )}
+                    <Route path={`/contacts/:id/private`}
+                           render={ (props) => <ContactPrivate {...props} user={contact} /> } />
+
                 </Fragment>
             ) : (
                 <p>Waiting for contact details</p>
